27123f3158aa8994b1c68cd9166e091fa1d35bc2
Leroy Zwakman <leroy.zwakman@gmail.com>
2022-03-12 13:34:59 +0100

    config file systemd

--- .config/bash/51_ssh.sh
+++ .config/bash/51_ssh.sh
@@ -5,3 +5,25 @@ export SSH_AUTH_SOCK
 if [[ `ssh-add -L | grep ssh | wc -l` -eq 0 ]]; then
    ssh-add -vvv
 fi
+
+## Instructions 
+
+#mkdir -p ~/.config/systemd/user/
+
+#cat > ~/.config/systemd/user/ssh-agent.service << "EOF"
+#[Unit]
+#Description=OpenSSH private key agent
+#IgnoreOnIsolate=true
+#
+#[Service]
+#Type=forking
+#Environment=SSH_AUTH_SOCK=%t/ssh-agent.socket
+#ExecStart=/usr/bin/ssh-agent -a $SSH_AUTH_SOCK
+#ExecStartPost=/usr/bin/systemctl --user set-environment SSH_AUTH_SOCK=${SSH_AUTH_SOCK}
+#
+#[Install]
+#WantedBy=default.target
+#EOF
+
+#systemctl --user enable --now ssh-agent.service
+
