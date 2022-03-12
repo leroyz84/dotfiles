db65ded7c657b738f0a033e5c4afceb6dca9998f
leroy.zwakman <leroy.zwakman@topicus.nl>
2022-03-12 13:16:23 +0100

    bash alias and color

--- .config/bash/settings.sh
+++ .config/bash/settings.sh
@@ -2,8 +2,11 @@
 
 #Allow 'reverse ctrl+r' ctrl + s search
 stty -ixon
+
+#https://www.gnu.org/software/bash/manual/html_node/The-Shopt-Builtin.html
 shopt -s autocd
 shopt -s histappend
+shopt -s expand_aliases
 
 export HISTCONTROL=ignoredups
 export HISTFILESIZE=
