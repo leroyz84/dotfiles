ba2ed6bc1f67cbedd670402edd4d4f0736486b92
Leroy Zwakman <leroy.zwakman@gmail.com>
2022-03-13 17:43:14 +0100

    font and terminal settings

--- .config/bash/10_colors.sh
+++ .config/bash/10_colors.sh
@@ -79,7 +79,7 @@ colors() {
 	done
 }
 
-export TERM=xterm-color
+#export TERM=xterm-color
 export GREP_OPTIONS='--color=auto' GREP_COLOR='1;32'
 export CLICOLOR=1
 export LSCOLORS=ExFxCxDxBxegedabagacad
--- .config/bash/60-i3-specific.sh
+++
@@ -1,2 +0,0 @@
-alias i3-config='vim ~/.config/i3/config'
-alias i3-keys='grep -Po "bindsym [0-9A-Za-z\+\$].+"  ~/.config/i3/config'
