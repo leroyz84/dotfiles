4dd83066918c97a499fcc259987e184e28b4024d
leroy.zwakman <leroy.zwakman@topicus.nl>
2022-03-13 15:11:40 +0100

    screenlayout

--- .config/i3/conf.d/monitor/screenlayouts/dual-head.sh
+++ .config/i3/conf.d/monitor/screenlayouts/dual-head.sh
@@ -12,6 +12,9 @@ if [[ ${LOCATION} == "THUIS" ]]; then
     xrandr --output DP-1-1 --mode 1920x1080 --pos 0x0 --rotate normal --primary
     #links
     xrandr --output DP-1-2 --mode 1920x1080 --rotate normal --left-of DP-1-1
+    #audio via hdmi:
+    pacmd set-card-profile 2 "output:hdmi-stereo"
+
 else
     echo "somewhere else"
     xrandr --output eDP-1 --mode 1920x1080 --pos 1920x0 --rotate normal
--- .config/i3/conf.d/monitor/screenlayouts/triple-head.sh
+++ .config/i3/conf.d/monitor/screenlayouts/triple-head.sh
@@ -14,7 +14,8 @@ if [[ ${LOCATION} == "THUIS" ]]; then
     xrandr --output DP-1-2 --mode 1920x1080 --pos 1920x0 --rotate normal
     #rechts
     xrandr --output DP-1-1 --mode 1920x1080 --pos 3840x0 --rotate normal --primary
-
+    #audio via hdmi
+    pacmd set-card-profile 2 "output:hdmi-stereo"
 else
     echo "somewhere else"
     #links HDMI
