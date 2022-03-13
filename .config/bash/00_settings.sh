a33401920b21db1b0063e68a1d1c5b4ae4c84187
Leroy Zwakman <leroy.zwakman@gmail.com>
2022-03-13 17:43:29 +0100

    font and prompt

---
+++ .config/bash/00_settings.sh
@@ -0,0 +1,18 @@
+# vim: set filetype=bash:
+
+#Allow 'reverse ctrl+r' ctrl + s search
+stty -ixon
+
+#https://www.gnu.org/software/bash/manual/html_node/The-Shopt-Builtin.html
+shopt -s autocd
+shopt -s histappend
+shopt -s expand_aliases
+
+export HISTCONTROL=ignoredups
+export HISTFILESIZE=
+export HISTSIZE=
+export PATH=$PATH:~/scripts
+
+export BROWSER=chromium
+export EDITOR='vim'
+export VISUAL='vim'
---
+++ .config/bash/99_alacritty.sh
@@ -0,0 +1,11 @@
+export WINIT_X11_SCALE_FACTOR=1.66 
+case ${TERM} in
+
+  xterm*|rxvt*|Eterm|aterm|kterm|gnome*)
+     PROMPT_COMMAND=${PROMPT_COMMAND:+$PROMPT_COMMAND; }'printf "\033]0;%s@%s:%s\007" "${USER}" "${HOSTNAME%%.*}" "${PWD/#$HOME/\~}"'
+
+    ;;
+  screen*)
+    PROMPT_COMMAND=${PROMPT_COMMAND:+$PROMPT_COMMAND; }'printf "\033_%s@%s:%s\033\\" "${USER}" "${HOSTNAME%%.*}" "${PWD/#$HOME/\~}"'
+    ;;
+esac
