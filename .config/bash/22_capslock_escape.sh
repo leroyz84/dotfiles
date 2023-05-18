0b1827079500798cd8803d09aefbf1cd61cf448a
Leroy Zwakman <leroy@p5540>
2023-05-18 14:20:18 +0200

    not used

--- .config/bash/22_capslock_escape.sh
+++ .config/bash/22_capslock_escape.sh
@@ -1,8 +1,8 @@
 #setxkbmap -option caps:escape
 # i use a duck xs61 keyboard, make escape a tilde :)
 # is now done by qmk.
-
-if  [[ ! `lsusb | grep XS61` ]]; then
-    xmodmap -e 'keycode 9 = grave asciitilde'
-fi
-
+#
+#if  [[ ! `lsusb | grep XS61` ]]; then
+#    xmodmap -e 'keycode 9 = grave asciitilde'
+#fi
+#
