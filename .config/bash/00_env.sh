64c5f0dd9ce5d2171a6ce6d943075d2345e4565a
Leroy Zwakman <leroy.zwakman@bcnexxt.com>
2023-07-10 19:56:50 +0200

    connect to virsh

--- .config/bash/00_env.sh
+++ .config/bash/00_env.sh
@@ -2,3 +2,4 @@ export LF_BOOKMARK_PATH=~/.config/lf/bookmarks
 if [[ -e ~/.config/phrases/00-temporary-password ]]; then
 	export SSHPASS=`cat ~/.config/phrases/00-temporary-password`
 fi
+ export LIBVIRT_DEFAULT_URI="qemu:///system"
