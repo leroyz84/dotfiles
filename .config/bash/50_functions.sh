64c7e90620e4a238af58c7e029b9825371d9cb70
Leroy Zwakman <leroy.zwakman@bcnexxt.com>
2023-07-27 16:33:06 +0200

    unused le

--- .config/bash/50_functions.sh
+++ .config/bash/50_functions.sh
@@ -2,12 +2,6 @@
 
 
 
-# edit the current terminal, was used with termite
-function le() {
-  xdotool key Print
-  tmp_file="/tmp/`ls -1rt /tmp | tail -1`"
-  vim +1 ${tmp_file}
-}
 
 #  bash completion
 [ -r /usr/share/bash-completion/bash_completion ] && . /usr/share/bash-completion/bash_completion
