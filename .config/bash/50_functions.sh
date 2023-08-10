73e603649cc8f13826db19c51385adf114aec6b9
Leroy Zwakman <leroy.zwakman@bcnexxt.com>
2023-08-10 08:10:53 +0200

    clean

--- .config/bash/50_functions.sh
+++ .config/bash/50_functions.sh
@@ -5,3 +5,8 @@
 
 #  bash completion
 [ -r /usr/share/bash-completion/bash_completion ] && . /usr/share/bash-completion/bash_completion
+
+clean_ssh () {
+	 sed -i -e '/dev.local/d' ~/.ssh/known_hosts
+	 sed -i -e '/tux.lan/d' ~/.ssh/known_hosts
+}
